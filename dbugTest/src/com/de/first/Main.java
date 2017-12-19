338b1a74858fa6ab84ff799de1b1901099738dfd
Chuman <Chuman@192.168.0.18>
2017-12-19 15:40:06 +0530

    Change-Id: Ic4b2395a091c71bdfe7a0489a4edf499e4a3a1e1 Signed-off-by: Chuman <Chuman@192.168.0.18>

---
+++ dbugTest/src/com/de/first/Main.java
@@ -0,0 +1,10 @@
+package com.de.first;
+
+public class Main {
+	public static void main(String[] args) {
+	    Counter counter = new Counter();
+	    counter.count();
+	    System.out.println("We have counted "
+	        + counter.getResult());
+	  }
+}
